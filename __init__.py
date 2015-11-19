f85694c084a2d99d23e6b5cb26f95780058bcb93
Andrey Kvichanskiy <kvichans@mail.ru>
2015-11-19 11:23:31 +0300

    ver 0.9.1

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@
 Authors:
     Andrey Kvichansky    (kvichans on githab)
 Version:
-    '0.8.0 2015-11-18'
+    '0.9.1 2015-11-19'
 '''
 
-import  os
+import  os, shutil, webbrowser
 import  cudatext        as app
 from    cudatext    import ed
 import  cudatext_cmd    as cmds
@@ -25,15 +25,23 @@ PROC_MENU_RECENTS   = 'recents'
 PROC_MENU_THEMES    = 'themes'
 PROC_MENU_PLUGINS   = 'plugins'
 
+DEF_MENU_CFG_FILE   = 'menu.json'
+REF_HELP_CFG_MENU   = 'https://github.com/kvichans/cuda_config_menu/wiki/Help-for-config-menu-with-JSON-files'
+
 # Localization
-MENU_NO_FILE        = 'No menu config file "{}"'
-MENU_NO_PRE_ID      = 'Skip config for id="{}" - no such main submenu'
-MENU_CONFIG_OK      = 'Reconfig menus from "{}"'
+LC_MENU_NO_FILE     = 'No menu config file "{}"'
+LC_MENU_NO_PRE_ID   = 'Skip config for id="{}" - no such main submenu'
+LC_MENU_CONFIG_OK   = 'OK config menus from "{}"'
+LC_MENU_CONFIG_FILE = 'Config file name'
+LC_CONFIG_ON_START  = 'Do config on start (Y/N)'
+LC_CONFIG_ON_FOCUS  = 'Do config on focus (Y/N)'
 
 pass;                           # Logging
 pass;                           from pprint import pformat
 pass;                           pfrm15=lambda d:pformat(d,width=15)
 pass;                           LOG = (-2==-2)  # Do or dont logging.
+
+last_file_cfg       = ('', 0)
 
 def _reset_menu(mn_prnt_id, mn_items):
     pass;                       #LOG and apx.log('>>mn_prnt_id, mn_items={}',(mn_prnt_id, pfrm15(mn_items)))
@@ -75,10 +83,26 @@ def config_menus(mn_cfg_json=''):
             "cmd": <int command code>|<py-module,method>    (for cmd-item, ignored if "cap"=="-")
             "sub":[<config-item> ,<config-item> ...]        (for submenu, ignored if "cmd")
     '''
-    mn_cfg_json = apx.get_opt('config_menus_from', mn_cfg_json) if not mn_cfg_json else mn_cfg_json
+    global last_file_cfg
+    mn_cfg_json = apx.get_opt('config_menus_from', DEF_MENU_CFG_FILE) if not mn_cfg_json else mn_cfg_json
     pass;                       #LOG and apx.log('mn_cfg_json={}',mn_cfg_json)
-    if not mn_cfg_json:    return app.msg_status(MENU_NO_FILE.format(mn_cfg_json))
+    if not mn_cfg_json:    
+        return app.msg_status(LC_MENU_NO_FILE.format(mn_cfg_json))
     mn_cfg_json = os.path.join(app.app_path(app.APP_DIR_SETTINGS), mn_cfg_json)
+    if not os.path.exists(mn_cfg_json):
+        last_file_cfg   = ('', 0)
+        return app.msg_status(LC_MENU_NO_FILE.format(mn_cfg_json))
+
+    # Try to skip config as the json is same as the last applied
+    curr_file_cfg   =   (mn_cfg_json
+                       , os.path.getmtime(mn_cfg_json))
+    skip            = ( (curr_file_cfg[0] == last_file_cfg[0]) 
+                    and (curr_file_cfg[1] <= last_file_cfg[1]) )
+    if skip:
+        pass;                  #LOG and apx.log('Skip as same as last mn_cfg_json={}',mn_cfg_json)
+        return
+    last_file_cfg   =    curr_file_cfg
+
     mn_cfg      = apx._json_loads(open(mn_cfg_json).read())
     pass;                       #LOG and apx.log('mn_cfg={}',pfrm15(mn_cfg))
     pre_list    = (PROC_MENU_TOP
@@ -92,20 +116,71 @@ def config_menus(mn_cfg_json=''):
                   ,PROC_MENU_TEXT)
     for mn_pre_id, mn_pre in mn_cfg.items():
         if mn_pre_id not in pre_list:
-            app.msg_status(MENU_NO_PRE_ID.format(mn_pre_id))
+            app.msg_status(LC_MENU_NO_PRE_ID, _PRE_ID.format(mn_pre_id))
             continue # for mn_pre_id
         if mn_pre.get('how', 'add') == 'clear':
             app.app_proc(app.PROC_MENU_CLEAR, mn_pre_id)
         _reset_menu(mn_pre_id, mn_pre.get('sub', []))
-    print(         MENU_CONFIG_OK.format(mn_cfg_json))
-    app.msg_status(MENU_CONFIG_OK.format(mn_cfg_json))
+    print(         LC_MENU_CONFIG_OK.format(mn_cfg_json))
+    app.msg_status(LC_MENU_CONFIG_OK.format(mn_cfg_json))
    #def config_menus
 
 class Command:
     def on_start(self, ed_self):
-        config_menus()
+        if not apx.get_opt('config_menus_1st_done', False):
+            # Actions on first run
+            mn_cfg_src  = os.path.join(os.path.dirname(__file__)         , DEF_MENU_CFG_FILE)
+            mn_cfg_trg  = os.path.join(app.app_path(app.APP_DIR_SETTINGS), DEF_MENU_CFG_FILE)
+            pass;              #LOG and apx.log('mn_cfg_src, mn_cfg_trg={}',(mn_cfg_src, mn_cfg_trg))
+            if not os.path.exists(mn_cfg_trg):
+                shutil.copy(mn_cfg_src, mn_cfg_trg)
+            apx.set_opt('config_menus_1st_done', True)
+        if apx.get_opt('config_menus_on_start', True):
+            config_menus()
        #def on_start
+
+#   def on_open(self, ed_self):
+#       pass;                  #LOG and apx.log('')
+#       config_menus()
+#      #def on_open
+
+    def on_focus(self, ed_self):
+        pass;                  #LOG and apx.log('')
+        if apx.get_opt('config_menus_on_focus', False):
+            config_menus()
+       #def on_focus
+
     def config_menus(self):
         config_menus()
        #def config_menus
+
+    def config_menus_open(self):
+        cfg_json    = apx.get_opt('config_menus_from', DEF_MENU_CFG_FILE)
+        cfg_json    = os.path.join(app.app_path(app.APP_DIR_SETTINGS), cfg_json)
+        app.file_open(cfg_json)
+       #def config_menus_open
+
+    def config_menus_help(self):
+        webbrowser.open_new_tab(REF_HELP_CFG_MENU)
+        app.msg_status('Opened in browser')
+       #def config_menus_help
+
+    def config_menus_settings(self):
+        cfg_file    = apx.get_opt('config_menus_from', DEF_MENU_CFG_FILE)
+        cfg_on_start= apx.get_opt('config_menus_on_start', True)
+        cfg_on_focus= apx.get_opt('config_menus_on_focus', False)
+        anses       = app.dlg_input_ex(3, "Settings for 'Config menu'"
+                        , LC_MENU_CONFIG_FILE, cfg_file
+                        , LC_CONFIG_ON_START, 'Y' if cfg_on_start else 'N'
+                        , LC_CONFIG_ON_FOCUS, 'Y' if cfg_on_focus else 'N'
+                        )
+        if anses is None:
+            return
+        if anses[0]!=cfg_file:
+            apx.set_opt('config_menus_from', anses[0])
+        if ('Y'==anses[1])!=cfg_on_start:
+            apx.set_opt('config_menus_on_start', ('Y'==anses[1]))
+        if ('Y'==anses[2])!=cfg_on_focus:
+            apx.set_opt('config_menus_on_focus', ('Y'==anses[2]))
+       #def config_menus_settings
    #class Command
--- readme/readme.txt
+++ readme/readme.txt
@@ -4,10 +4,98 @@ Default config file is
 	settings\menu.json
 Other file can uses by opt
 	"config_menus_from"
-Example:
-	In file settings\user.json
-		...
-		"config_menus_from":"my-menu.json",
-		...
 
 Author: Andrey Kvichanskiy (kvichans, at forum)
+
+
+==========
+What parts of menus can be configured?
+==========
+CudaText (for version "0.9.8") allows:
+    Change all items of main menu (File/Edit/...).
+    Change all items of File except submenu Open recent.
+    Change all items of Edit, Selection, Search, View.
+    No changeable items in Plugins, Options, Help (but its are complemented).
+    Change all items of local menu for main text.
+    No changeable items in other local menus.
+
+Change all means that you can rename/skip/move all native items and can add any others. 
+Indeed you can at first clear all native items and then to fill with needed. Or save native and add needed to the end.
+
+Three submenus (Plugins, Options, Help) dont allow to clear but allow to add new items.
+
+Filling likes to create a tree as you can add not only commands but submenus too. 
+Note that some submenus are autofilled:
+    "recents" - recent-files submenu (natively in File)
+    "themes" - Color-themes submenu (natively in Options)
+    "plugins" - Plugins submenu (natively as item of main menu)
+
+Need commands you can find in two collections:
+    Part of native commands (see names into py\cudatext_cmd.py start with cmd_ or cCommand_)
+    All plugin commands (see install.inf files for all installed plugin or/and items of settings\plugins.json)
+
+==========
+Settings
+==========
+After plugin installation (and once app start) file menu.json appears in directory settings. 
+It allows to repeat (not all but almost) native menus and it is good to start experiments.
+
+Settings for plugin:
+    Option "config_menus_from" point to config file name (as "menu.json") which will be searched into directory settings.
+    Option "config_menus_on_start" with values true/false to apply config file when app starts.
+    Option "config_menus_on_focus" with values true/false to apply config file when text editor gets focus. 
+		Only true allows to use adaptive menus (lexer-menus).
+
+Better to rename file menu.json to my-menu.json and to write new value in user.json for key "config_menus_from". 
+Source menu.json can be updated in the future with upgrade of plugin/app.
+
+Note that key "config_menus_from" can has different values for different lexers. 
+Thus you can use lexer-menus. 
+For this you can open current lexer settings (Options/Settings - more/Settings - lexer overide) and set value for key "config_menus_from".
+
+Example:
+    In settings/user.json
+        "config_menus_from":"my-menu.json" (all my menus)
+    In settings/lexer Python.json
+        "config_menus_from":"menu Python.json" (changes for menus to work with Python)
+    In settings/lexer JSON.json
+        "config_menus_from":"menu JSON.json" (changes for menus to work with JSON)
+
+==========
+JSON file format
+==========
+Brief view of file content:
+{
+  "top-file":{"how":"clear", ...all new items for native File menu...},
+  "top-help":{"how":"add", ...additional items for native Help menu...},
+}
+
+Notes
+    Root keys (like "top-file" and "top-help" above) must be only from the list
+        "top","top-file","top-edit","top-sel","top-sr","top-view","top-op","top-help","text"
+    You can use any subset of the list. Lexer-menus can rebuild only context menu ("text").
+    Root key "top" uses to rebuild main menu. You can use other names (Find instead Search), change order, skip something (Help?) or add new items.
+    Root key "text" uses to rebuild context (local) menu for editing text.
+    Pair "how":"clear" uses to fill as empty. It is default.
+    Pair "how":"add" uses to fill at end.
+
+Brief view of one branch:
+  "top-file":{
+    sub:[
+      {"cap":"Save file", "cmd":"cmd_FileSave"},
+      {"cap":"Lines", 
+       "sub":[
+         {"cap":"Trim all", "cmd":"cCommand_TextTrimSpacesAll"},
+         {"cap":"-"},
+         {"cap":"My Split","cmd":"my_plug,split"},
+       ],
+      },
+    ],
+  },
+
+Notes
+    Value for key "cap" is caption for item or submenu.
+    Pair "cap":"-" uses to add menu separator.
+    Values for key "cmd" are
+        names from py\cudatext_cmd.py (start with cmd_ or cCommand_),
+        strings "module,method" for any plugin module and method in it.
